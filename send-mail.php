678c78a852f3f7abf70654295d074d2b26fc65e8
jlmudarra <joanlluismudarra@gmail.com>
2020-07-19 20:33:30 +0200

    v.0.999

--- send-mail.php
+++ send-mail.php
@@ -11,44 +11,14 @@
 	$subject = trim( $_POST['subject'] );
 	$message = trim( $_POST['message'] );
 	
-	// check for error
-	$error = false;
-	if ( $name === "" ) { $error = true; }
-	if ( $email === "" ) { $error = true; }
-	if ( $subject === "" ) { $error = true; }
-	if ( $message === "" ) { $error = true; }
 	
-	// anti-spam check
-	// http://nfriedly.com/techblog/2009/11/how-to-build-a-spam-free-contact-forms-without-captchas/
-	// if the url field is empty 
-	if(isset($_POST['url']) && $_POST['url'] == ''){
-		 
-		 // if no error, then send mail
-		if ( $error == false )
-		{
-			$body = "Name: $name \n\nEmail: $email \n\nMessage: $message";
-			
-			$headers = "From: " . $site_name . ' <' . $sender_domain . '> ' . "\r\n";
-			$headers .= "Reply-To: " . $name . ' <' . $email . '> ' . "\r\n";
-			
-			$mail_result = mail( $to, $subject, $body, $headers );
-			
-			if ( $mail_result == true )
-				{ echo 'success'; }
-			else
-				{ echo 'unsuccess'; }
-		}
-		else // not validated
-		{
-			echo 'error';
-		}
-		// end if
-		 
-	}
-	else // BOT DETECTED - lets lie to it 
-	{
-		//echo "Thanks, We'll get back to you as soon as possible";
-		echo 'success';
-	}
+	$para      = 'joanlluismudarra@gmail.com';
+	$titulo    = 'Message From - ' . $email;
+	$mensaje   = $name . ' - ' .$message;
+	$cabeceras = 'From: webmaster@example.com' . "\r\n" .
+		'Reply-To: webmaster@example.com' . "\r\n" .
+		'X-Mailer: PHP/' . phpversion();
+	
+	mail($para, $titulo, $mensaje, $cabeceras);
 	
 ?>
